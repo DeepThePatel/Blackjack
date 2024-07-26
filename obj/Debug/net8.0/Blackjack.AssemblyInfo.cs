e612c97e9bf1c2347692095e8d254baa584d9396
Deep Patel <deepkalpana1@aol.com>
2024-07-26 18:49:30 -0400

    created wip branch

--- obj/Debug/net8.0/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d75a39367275396a1ff7b87051c2cf17d9e02076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd339800e96cad15f535dff1174511b17283bfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
