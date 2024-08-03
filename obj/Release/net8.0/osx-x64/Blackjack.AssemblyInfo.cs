c6214eef4da4418fb697c1da3438bdd8384f042f
Deep Patel <deepkalpana1@aol.com>
2024-08-02 20:31:16 -0400

    osx version

--- obj/Release/net8.0/osx-x64/Blackjack.AssemblyInfo.cs
+++ obj/Release/net8.0/osx-x64/Blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c71ebd50906718cc53031034c0d1f975972f2852")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8c2570e452e28f3d27d7f038bfe4ab02feb468")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
