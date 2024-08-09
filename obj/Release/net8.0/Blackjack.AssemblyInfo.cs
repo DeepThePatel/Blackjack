b8e2d030a8563b843111766c904cee7655a079e9
Deep Patel <deepkalpana1@aol.com>
2024-08-09 17:15:19 -0400

    minor changes

--- obj/Release/net8.0/Blackjack.AssemblyInfo.cs
+++ obj/Release/net8.0/Blackjack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+384219771d066482146e7b4768fd63f8f19db2ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69dca793ae76e935d07f66f0fc4144fc3c04194")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
